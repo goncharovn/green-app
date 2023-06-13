b9a7cbfff9d4cf1696ebbc49410b363e25ada030
goncharovn <tikagoncharov@gmail.com>
2023-06-14 00:43:03 +0700

    feat: style authForm

--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
 import { greenApi } from 'app/greenApi'
 import { useAppDispatch } from 'app/hooks'
-import { authorize } from './authSlice';
+import { authorize } from './authSlice'
+import './style.scss'
 
 const Auth = () => {
 	const [idInstance, setIdInstance] = useState('')
@@ -11,8 +12,10 @@ const Auth = () => {
 
 	const [trigger] = greenApi.useLazyGetStateInstanceQuery()
 
-	const handleClick = async () => {
+	const handleClick = async (event: SyntheticEvent) => {
 		try {
+			event.preventDefault();
+
 			const result = await trigger({ idInstance, apiTokenInstance }).unwrap()
 
 			if (result.stateInstance === 'authorized') {
@@ -24,27 +27,40 @@ const Auth = () => {
 					})
 				)
 			}
-		} catch (e) {
-			console.log(e)
+		} catch (error) {
+			console.log(error)
 		}
 	}
 
 	return (
-		<>
-			<div>
+		<div
+			className='authFormWrapper'
+		>
+			<form
+				className='authForm'
+			>
+				<span className='authForm__hint'>Чтобы воспользоваться сервисом, зарегистрируйтесь <a href="https://console.green-api.com/auth/register">здесь</a>.</span>
 				<input
+					className='input'
 					value={idInstance}
 					onChange={(e) => setIdInstance(e.target.value)}
 					placeholder="idInstance"
+					autoFocus
 				/>
 				<input
+					className='input'
 					value={apiTokenInstance}
 					onChange={(e) => setApiTokenInstance(e.target.value)}
 					placeholder="apiTokenInstance"
 				/>
-				<button onClick={handleClick}>Войти</button>
-			</div>
-		</>
+				<button
+					className='button authForm__button button--green'
+					onClick={handleClick}
+				>
+					Войти
+				</button>
+			</form>
+		</div>
 	)
 }
 
